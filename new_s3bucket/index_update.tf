dfe45bf186e635484c641d141bd6f67fe5394e8f
na-rong <129728196+na-rong@users.noreply.github.com>
2024-10-16 12:25:40 +0900

    Delete new_s3bucket directory

--- new_s3bucket/index_update.tf
+++
@@ -1,7 +0,0 @@
-# 수정된 index.html 파일을 S3에 다시 업로드
-resource "aws_s3_object" "updated_index_html" {
-  bucket        = aws_s3_bucket.bucket2.id  # S3 버킷 이름
-  key           = "index.html"              # 업로드할 파일 이름
-  source        = "index.html"              # 수정된 로컬 index.html 파일 경로
-  content_type  = "text/html"
-}
--- new_s3bucket/main_upload.tf
+++
@@ -1,7 +0,0 @@
-# 새로운 main.html 파일을 S3에 업로드
-resource "aws_s3_object" "main_html" {
-  bucket        = aws_s3_bucket.bucket2.id  # S3 버킷 이름 지정
-  key           = "main.html"               # S3에 업로드할 파일 이름
-  source        = "main.html"               # 로컬 경로에서 main.html 파일 지정
-  content_type  = "text/html"               # 파일 타입 설정
-}
